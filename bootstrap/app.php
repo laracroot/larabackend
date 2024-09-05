8fb4d5b05e6d28bf244154ffc244a8d4334af3c4
Rolly Maulana  Awangga <rolly@awang.ga>
2024-09-05 18:13:40 +0700

    api/users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //disable csrf pada api
         $middleware->validateCsrfTokens(except: [
-            'https://bukupedia.alwaysdata.net/api/users' // <-- exclude this route
+            'api/users' // <-- exclude this route
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
