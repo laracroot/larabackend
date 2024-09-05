aed65ee6fdb56d4c08e36f3ef60980b39e771ecf
Rolly Maulana  Awangga <rolly@awang.ga>
2024-09-05 18:17:33 +0700

    4 exclude

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: '/api',//prefix untuk api
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
         //disable csrf pada api
         $middleware->validateCsrfTokens(except: [
-            'api/users' // <-- exclude this route
+            'api/users', // <-- exclude this route
+            'api/*',
+            'api/users/*',
+            'users/*',
         ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
     })->create();
