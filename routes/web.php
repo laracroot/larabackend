e1aac49d27bae72888a0790d67796a63458a6117
Rolly Maulana  Awangga <rolly@awang.ga>
2024-09-05 17:51:45 +0700

    web route return json

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
+Route::any('/', function () {
+    return response()->json(['message' => 'Welcome to the API'], 200);
+});
