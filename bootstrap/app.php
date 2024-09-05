a086009643444be498aaef8c119f25dcaf9df7f9
Rolly Maulana  Awangga <rolly@awang.ga>
2024-09-05 18:22:51 +0700

    users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/*',
             'api/users/*',
             'users/*',
+            'users',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
