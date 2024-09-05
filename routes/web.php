7ba40024a8e248af8c1682cefd18ae40251f408d
Rolly Maulana  Awangga <rolly@awang.ga>
2024-09-05 18:00:38 +0700

    komentar web route semua

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
+/* use Illuminate\Support\Facades\Route;
+ */
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
-Route::any('/', function () {
+/* Route::any('/', function () {
     return response()->json(['message' => 'Welcome to the API'], 200);
-});
+}); */
